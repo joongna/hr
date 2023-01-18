1e0575d10a5ec2e765a90b2a2308739ca3c2ac5b
Myunghwan Kim <samkim2626@gmail.com>
2023-01-19 01:53:36 +0900

    test 관련 의존성 추가(kotest, mockk)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,14 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.mysql:mysql-connector-j")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(module = "mockito-core")
+	}
 	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 	testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+	testImplementation("io.kotest:kotest-assertions-core:5.5.4")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+	testImplementation("com.ninja-squad:springmockk:4.0.0")
 }
 
 tasks.withType<KotlinCompile> {
